f5d9eec190c2a22d48f5061fca5826860d44648c
K. Adam Christensen <pope@shifteleven.com>
2022-08-20 12:38:36 -0700

    Take my examples and put it into an observable.

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,77 @@
-console.log('Hello, world');
+import { concat, debounceTime, distinctUntilChanged, filter, from, fromEvent, map, of, scan, switchMap } from 'rxjs';
 
-async function trySomething(): Promise<void> {
-    const midiAccess = await navigator.requestMIDIAccess();
-    //handleUpdates(midiAccess);
-    midiAccess.addEventListener('statechange', (event) => {
-        console.log(event);
-        handleUpdates(midiAccess);
-    });
+const CC_MIDI_COMMAND = 0xB0;
+
+interface NymphesPorts {
+    readonly midiAccess: WebMidi.MIDIAccess;
+    readonly input: WebMidi.MIDIInput;
+    readonly output: WebMidi.MIDIOutput;
 }
 
-function handleUpdates(midiAccess: WebMidi.MIDIAccess): void {
-    printInputs(midiAccess);
-    printOutputs(midiAccess);
+const midiAccess$ = from(navigator.requestMIDIAccess());
 
-    const input = getNymphesInput(midiAccess);
-    input.onmidimessage = (event) => {
-        let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
-        for (const character of event.data) {
-            str += `0x${character.toString(16)} `;
+const nymphesPorts$ = midiAccess$.pipe(
+    switchMap((midiAccess) => concat(
+        of(getNymphesPorts(midiAccess)),
+        fromEvent(midiAccess, 'statechange').pipe((map(() => getNymphesPorts(midiAccess)))))),
+    distinctUntilChanged((previous, current) => {
+        if (previous === current) {
+            return true;
         }
-        console.log(str);
+        if (previous === undefined || current === undefined) {
+            return false;
+        }
+        // TODO(pope): Do a better equality check.
+        return previous.input.id === current.input.id;
+    }),
+);
+
+const currentCcValues$ = nymphesPorts$.pipe(
+    switchMap((ports) => {
+        if (!ports) {
+            return of(null);
+        }
+        return fromEvent<WebMidi.MIDIMessageEvent>(ports.input, 'midimessage')
+    }),
+    filter((event) => !event || (event.data.length === 3 && event.data.at(0) === CC_MIDI_COMMAND)),
+    scan((state, event) => {
+        if (!event) {
+            // Reset
+            return {};
+        }
+        return {
+            ...state,
+            [event.data.at(1)!]: event.data.at(2)!,
+        };
+    }, {} as { [key: number]: number }),
+)
+
+function getNymphesPorts(midiAccess: WebMidi.MIDIAccess): NymphesPorts | undefined {
+    let input: WebMidi.MIDIInput | undefined;
+    let output: WebMidi.MIDIOutput | undefined;
+
+    for (const port of midiAccess.inputs.values()) {
+        if (port.manufacturer === 'STMicroelectronics' && port.name === 'Nymphes') {
+            input = port;
+            break;
+        }
+    }
+    for (const port of midiAccess.outputs.values()) {
+        if (port.manufacturer === 'STMicroelectronics' && port.name === 'Nymphes') {
+            output = port;
+            break;
+        }
+    }
+    if (!input || !output) {
+        return undefined;
+    }
+    return {
+        midiAccess,
+        input,
+        output,
     };
-    console.log(input);
 }
 
-function printInputs(midiAccess: WebMidi.MIDIAccess): void {
-    console.log(midiAccess.inputs.size);
-    midiAccess.inputs.forEach((input, key) => {
-        console.log(`Key: ${key}`);
-        console.log(`Input port [type:'${input.type}']` +
-            ` id:'${input.id}'` +
-            ` manufacturer:'${input.manufacturer}'` +
-            ` name:'${input.name}'` +
-            ` version:'${input.version}'`);
-    });
-}
-
-function printOutputs(midiAccess: WebMidi.MIDIAccess): void {
-    console.log(midiAccess.inputs.size);
-    midiAccess.outputs.forEach((output, key) => {
-        console.log(`Key: ${key}`);
-        console.log(`Output port [type:'${output.type}']` +
-            ` id:'${output.id}'` +
-            ` manufacturer:'${output.manufacturer}'` +
-            ` name:'${output.name}'` +
-            ` version:'${output.version}'`);
-    });
-}
-
-function getNymphesInput(midiAccess: WebMidi.MIDIAccess): WebMidi.MIDIInput {
-    for (const input of midiAccess.inputs.values()) {
-        if (input.manufacturer === 'STMicroelectronics' && input.name === 'Nymphes') {
-            return input;
-        }
-    }
-    throw new Error('Unable to find Nymphes');
-}
-
-function getNymphesOutput(midiAccess: WebMidi.MIDIAccess): WebMidi.MIDIOutput {
-    for (const output of midiAccess.outputs.values()) {
-        if (output.manufacturer === 'STMicroelectronics' && output.name === 'Nymphes') {
-            return output;
-        }
-    }
-    throw new Error('Unable to find Nymphes');
-}
-
-trySomething();
+currentCcValues$
+    .pipe(debounceTime(5))
+    .subscribe((obj) => console.log(obj));
